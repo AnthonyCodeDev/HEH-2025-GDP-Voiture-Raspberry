28e8cdec7d205ca010a58651f746a01d72e690c1
Panda <46421954+PandaDouxYT@users.noreply.github.com>
2025-04-08 16:44:42 +0200

    Fix

--- feature_tour_en_8.py
+++ feature_tour_en_8.py
@@ -5,7 +5,7 @@ from moteur import MotorController
 from servo_controller import ServoController
 
 class TourEn8:
-    def __init__(self, speed=35, total_time=12, dt=0.02):
+    def __init__(self, speed=35, total_time=9, dt=0.02):
         self.motor = MotorController()
         self.servo = ServoController()
         self.speed = speed
