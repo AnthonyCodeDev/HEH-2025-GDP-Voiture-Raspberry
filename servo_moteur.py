43fe07ad21c6dac359828b150896a7173bfa7a7c
Panda <46421954+PandaDouxYT@users.noreply.github.com>
2025-04-08 13:50:31 +0200

    Fix

--- servo_moteur.py
+++ servo_moteur.py
@@ -3,30 +3,47 @@ import PCA9685 as PCA
 import time
 
 class ServoController:
+    """
+    Contrôleur de servo.
+
+    Auteur : Anthony Vergeylen
+    Date   : 08-04-2025
+    """
     def __init__(self, center=320, minimum=200, maximum=500):
-        self.servo = PCA.PWM()
-        self.servo.frequency = 60
+        self.pwm = PCA.PWM()
+        self.pwm.frequency = 60
         self.center_val = center
         self.min_val = minimum
         self.max_val = maximum
 
     def rotate(self, angle):
+        """
+        Positionne le servo à un angle compris entre -45° et 45°.
+        """
         angle = max(-45, min(45, angle))
         if angle > 0:
             pulse = self.center_val + ((angle / 45.0) * (self.max_val - self.center_val))
         else:
             pulse = self.center_val + ((angle / 45.0) * (self.center_val - self.min_val))
-        self.servo.write(0, 0, int(pulse))
+        self.pwm.write(0, 0, int(pulse))
 
-    def reset_position(self):
-        self.rotate(5)
+    def disable_pwm(self):
+        """
+        Désactive la sortie PWM pour ne pas maintenir le signal.
+        """
+        self.pwm.write(0, 0, 4096)
+
+def main():
+    """
+    Positionne le servo à 5° puis désactive la sortie PWM.
+    
+    Auteur : Anthony Vergeylen
+    Date   : 08-04-2025
+    """
+    servo = ServoController()
+    servo.rotate(5)
+    time.sleep(1)  # Attendre que le servo atteigne la position
+    servo.disable_pwm()
 
 if __name__ == "__main__":
-    servo = ServoController()
-    servo.reset_position()
-    # time.sleep(1)
-    # servo.rotate(45)
-    # time.sleep(1)
-    # servo.rotate(-45)
-    # time.sleep(1)
-    # servo.reset_position()
+    main()
