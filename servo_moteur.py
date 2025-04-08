f46d37fd4a838fc32eaff8dd0ff25d2d1507adc1
Panda <46421954+PandaDouxYT@users.noreply.github.com>
2025-04-08 13:32:38 +0200

    Fix

--- servo_moteur.py
+++ servo_moteur.py
@@ -3,30 +3,45 @@ import PCA9685 as PCA
 import time
 
 class ServoController:
+    """
+    Contrôleur de servo.
+
+    Auteur : Anthony Vergeylen
+    Date   : 08-04-2025
+    """
     def __init__(self, center=320, minimum=200, maximum=500):
-        self.servo = PCA.PWM()
-        self.servo.frequency = 60
-        self.center_val = center
-        self.min_val = minimum
-        self.max_val = maximum
+        self._servo = PCA.PWM()
+        self._servo.frequency = 60
+        self._center = center
+        self._min = minimum
+        self._max = maximum
 
-    def rotate(self, angle):
+    def move(self, angle):
+        """
+        Positionne le servo à un angle compris entre -45° et 45°.
+
+        :param angle: Angle souhaité en degrés.
+        """
         angle = max(-45, min(45, angle))
-        if angle > 0:
-            pulse = self.center_val + ((angle / 45.0) * (self.max_val - self.center_val))
+        if angle >= 0:
+            pulse = self._center + ((angle / 45.0) * (self._max - self._center))
         else:
-            pulse = self.center_val + ((angle / 45.0) * (self.center_val - self.min_val))
-        self.servo.write(0, 0, int(pulse))
+            pulse = self._center + ((angle / 45.0) * (self._center - self._min))
+        self._servo.write(0, 0, int(pulse))
 
-    def reset_position(self):
-        self.rotate(45)
+    def reset(self):
+        """
+        Positionne le servo à la valeur centrale pour éviter tout mouvement intempestif.
+        """
+        self._servo.write(0, 0, int(self._center))
 
 if __name__ == "__main__":
     servo = ServoController()
-    servo.reset_position()
+    servo.reset()
+    # Optionnel pour tester le mouvement :
     # time.sleep(1)
-    # servo.rotate(45)
+    # servo.move(45)
     # time.sleep(1)
-    # servo.rotate(-45)
+    # servo.move(-45)
     # time.sleep(1)
-    # servo.reset_position()
+    # servo.reset()
