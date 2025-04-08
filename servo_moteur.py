6817a6e71d39b4c7052d10157886290a0d79ad08
Panda <46421954+PandaDouxYT@users.noreply.github.com>
2025-04-08 13:36:22 +0200

    Fix

--- servo_moteur.py
+++ servo_moteur.py
@@ -3,24 +3,28 @@ import PCA9685 as PCA
 import time
 
 class ServoController:
-    """
-    Contrôleur de servo.
-
-    Auteur : Anthony Vergeylen
-    Date   : 08-04-2025
-    """
     def __init__(self, center=320, minimum=200, maximum=500):
         self._servo = PCA.PWM()
         self._servo.frequency = 60
         self._center = center
         self._min = minimum
         self._max = maximum
+        # Désactive la sortie PWM dès l'initialisation pour éviter les impulsions intempestives
+        self.disable_output()
+        time.sleep(0.1)
+        # Positionne le servo en position neutre
+        self.reset()
+
+    def disable_output(self):
+        """
+        Désactive la sortie PWM sur le canal utilisé.
+        En supposant que l'envoi de "off" = 4096 désactive la sortie.
+        """
+        self._servo.write(0, 0, 4096)
 
     def move(self, angle):
         """
         Positionne le servo à un angle compris entre -45° et 45°.
-
-        :param angle: Angle souhaité en degrés.
         """
         angle = max(-45, min(45, angle))
         if angle >= 0:
@@ -31,14 +35,13 @@ class ServoController:
 
     def reset(self):
         """
-        Positionne le servo à la valeur centrale pour éviter tout mouvement intempestif.
+        Positionne le servo à la valeur centrale (neutre).
         """
         self._servo.write(0, 0, int(self._center))
 
 if __name__ == "__main__":
     servo = ServoController()
-    servo.reset()
-    # Optionnel pour tester le mouvement :
+    # Optionnel : tester quelques mouvements après initialisation
     # time.sleep(1)
     # servo.move(45)
     # time.sleep(1)
