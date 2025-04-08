6d8dd9523774da23c28727ebacc698ec88328b08
Panda <46421954+PandaDouxYT@users.noreply.github.com>
2025-04-08 13:47:02 +0200

    Fix

--- servo_moteur.py
+++ servo_moteur.py
@@ -19,7 +19,7 @@ class ServoController:
         self.servo.write(0, 0, int(pulse))
 
     def reset_position(self):
-        self.rotate(45)
+        self.rotate(20)
 
 if __name__ == "__main__":
     servo = ServoController()
