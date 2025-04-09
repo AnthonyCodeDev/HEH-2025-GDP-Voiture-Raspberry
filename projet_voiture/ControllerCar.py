cc9c9de18efe7f6cffbb02bbc6c3b888e5281257
Panda <46421954+PandaDouxYT@users.noreply.github.com>
2025-04-09 14:29:17 +0200

    Fix

--- projet_voiture/ControllerCar.py
+++ projet_voiture/ControllerCar.py
@@ -37,17 +37,17 @@ class ControllerCar:
             return
 
         # Seuils de détection (en cm)
-        self.side_threshold = 15         # Obstacle latéral
+        self.side_threshold = 20         # Obstacle latéral
         self.front_threshold = 40        # Obstacle frontal (avertissement)
         self.emergency_threshold = 30    # Obstacle frontal (urgence)
 
         # Paramètres de virage
         self.angle_virage_gauche = -22.5
-        self.angle_virage_droite = 45
+        self.angle_virage_droite = 45 
         self.angle_central = 45
 
         # Durées (en secondes)
-        self.duree_virage = 1
+        self.duree_virage = 3
         self.duree_marche_arriere = 1
         self.reverse_pause = 0.5
 
