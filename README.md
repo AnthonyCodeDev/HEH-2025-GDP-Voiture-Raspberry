ab09ee5ae3cf99b1c972ef4664819a1e6bd0d65b
Rayan El khachani <rayan.elkhachani@std.heh.be>
2025-04-10 14:33:35 +0200

    test pour les capteurs rgb

--- README.md
+++
@@ -1,2 +0,0 @@
-# HEH-2025-Projet-Voiture
-HEH-2025-Projet-Voiture
--- testing/mock_rgb.py
+++ testing/mock_rgb.py
@@ -45,51 +45,6 @@ class TestCapteurRGB(unittest.TestCase):
         self.assertEqual(self.capteur.detect_color(50, 255, 50), "vert")
         self.assertEqual(self.capteur.detect_color(50, 50, 255), "bleu")
         self.assertEqual(self.capteur.detect_color(100, 100, 100), "indéterminé")
-    
-    @patch('projet_voiture.CapteurRGB.CarLauncher')
-    def test_monitor(self, mock_car_launcher):
-        """Test que la voiture est lancée lorsque la couleur verte est détectée"""
-
-        # Création d'une instance mock de CarLauncher
-        car_launcher_mock = MagicMock()
-        
-        # Simuler la couleur verte pour la détection
-        self.mock_sensor.color_rgb_bytes = (0, 255, 0)  # Vert (R=0, G=255, B=0)
-
-        # On simule un fonctionnement en parallèle de monitor
-        with patch('time.sleep', return_value=None):  # On empêche le vrai `time.sleep` pour accélérer les tests
-            monitor_thread = threading.Thread(target=self.capteur.monitor, args=(car_launcher_mock,))
-            monitor_thread.start()
-
-        # Attendre un petit peu pour s'assurer que le thread ait démarré
-        time.sleep(1)
-
-        # Vérifie que la méthode `launch` a été appelée sur `car_launcher`
-        car_launcher_mock.launch.assert_called_once()
-
-        # Arrêter le thread après le test
-        monitor_thread.join()
-
-    @patch('projet_voiture.CapteurRGB.CarLauncher')
-    def test_monitor_does_not_launch_on_non_green(self, mock_car_launcher):
-        """Test que la voiture n'est pas lancée si la couleur n'est pas verte"""
-
-        # Simuler une couleur rouge pour la détection
-        self.mock_sensor.color_rgb_bytes = (255, 0, 0)  # Rouge (R=255, G=0, B=0)
-
-        # On simule un fonctionnement en parallèle de monitor
-        with patch('time.sleep', return_value=None):  # On empêche le vrai `time.sleep` pour accélérer les tests
-            monitor_thread = threading.Thread(target=self.capteur.monitor, args=(mock_car_launcher,))
-            monitor_thread.start()
-
-        # Attendre un petit peu pour s'assurer que le thread ait démarré
-        time.sleep(1)
-
-        # Vérifie que la méthode `launch` n'a pas été appelée sur `car_launcher`
-        mock_car_launcher.launch.assert_not_called()
-
-        # Arrêter le thread après le test
-        monitor_thread.join()
 
 if __name__ == '__main__':
     unittest.main()
