91c3a7a66f51691c98bea0a368444a01124fb606
Mat_squat <diletomatteo@gmail.com>
2025-04-10 11:43:44 +0200

    Add fonction test for hcsr04

--- CheckSensorBeforeRace.py
+++ CheckSensorBeforeRace.py
@@ -4,7 +4,6 @@ import board
 import busio
 import adafruit_tcs34725
 from gpiozero import DistanceSensor
-import PCA9685 as PCA
 import sys
 
 # --- Vérification GPIO moteurs ---
@@ -52,17 +51,65 @@ def test_servo_moteur_basique():
         pwm.write(0, 0, 45)  # Retour centre
         time.sleep(0.3)
         pwm.write(0, 0, 4096)  # Désactive le signal
-        print(" Servo moteur (sans ServoController) : OK")
+        print(" Servo moteur : OK")
         return True
     except Exception as e:
         print(f" Servo moteur : ERREUR -> {e}")
         return False
 
+def test_hcsr04(TRIG, ECHO, place):
+    try:
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(TRIG, GPIO.OUT)
+        GPIO.setup(ECHO, GPIO.IN)
+
+        # S'assurer que TRIG est bas
+        GPIO.output(TRIG, False)
+        time.sleep(0.5)
+
+        # Envoi d’une impulsion TRIG
+        GPIO.output(TRIG, True)
+        time.sleep(0.00001)  # 10 µs
+        GPIO.output(TRIG, False)
+
+        # Mesure du temps de réponse sur ECHO
+        timeout = time.time() + 0.05  # 50 ms timeout
+
+        while GPIO.input(ECHO) == 0:
+            pulse_start = time.time()
+            if pulse_start > timeout:
+                raise TimeoutError("Aucune réponse du capteur (ECHO reste bas)")
+                return False
+
+        while GPIO.input(ECHO) == 1:
+            pulse_end = time.time()
+            if pulse_end - pulse_start > 0.025:  # au-delà de 4m (~25ms)
+                raise TimeoutError("Durée d'impulsion trop longue")
+                return False
+
+        pulse_duration = pulse_end - pulse_start
+        distance = pulse_duration * 17150  # cm
+        distance = round(distance, 2)
+
+        print(f"HC-SR04 {place} OK - Distance mesurée : {distance} cm")
+        return True
+
+    except Exception as e:
+        print(f"Capteur HC-SR04 {place} ERREUR : {e}")
+        return False
+
+    finally:
+        GPIO.cleanup()
+        
 def main():
-    print("Vérification des capteurs en cours...\n")
+    print("Vérification des capteurs en cours\n")
 
     gpio_ok = test_gpio_moteur([17, 18, 27, 22])
     rgb_ok = test_rgb_sensor()
+    hcsr_droit = test_hcsr04(26,19, "DROIT")
+    hcsr_avant = test_hcsr04(6,5, 'AVANT')
+    hcsr_gauche = test_hcsr04(11,9, 'GAUCHE')
+
 
 if __name__ == "__main__":
     main()
