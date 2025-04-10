23250bf140e7d764c9f6e9481b8949f1660f1344
Panda <46421954+PandaDouxYT@users.noreply.github.com>
2025-04-10 08:54:07 +0200

    1.1 Fix Modification des Classes en Fichiers

---
+++ projet_voiture/CarLauncher.py
@@ -0,0 +1,17 @@
+class CarLauncher:
+    """
+    Classe pour lancer le contrôle autonome de la voiture.
+
+    QUI: Vergeylen Anthony
+    QUOI: Utilise une instance existante de ControllerCar pour démarrer le contrôle autonome.
+    """
+    def __init__(self, car_controller):
+        self.car_controller = car_controller
+
+    def launch(self):
+        self.car_controller.run()
+
+    def shutdown(self):
+        print("🔒 Arrêt de la voiture en cours...")
+        self.car_controller.cleanup()
+        os._exit(0)
--- projet_voiture/main.py
+++ projet_voiture/main.py
@@ -17,25 +17,8 @@ import os
 from ControllerCar import ControllerCar
 from WebServerCar import VoitureServer
 from CapteurRGB import CapteurRGB
-from LineFollower import LineFollower
-
-class CarLauncher:
-    """
-    Classe pour lancer le contrôle autonome de la voiture.
-
-    QUI: Vergeylen Anthony
-    QUOI: Utilise une instance existante de ControllerCar pour démarrer le contrôle autonome.
-    """
-    def __init__(self, car_controller):
-        self.car_controller = car_controller
-
-    def launch(self):
-        self.car_controller.run()
-
-    def shutdown(self):
-        print("🔒 Arrêt de la voiture en cours...")
-        self.car_controller.cleanup()
-        os._exit(0)
+# from LineFollower import LineFollower
+from CarLauncher import CarLauncher
 
 class MainController:
     """
